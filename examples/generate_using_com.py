a1928cd0daa9202c3b353e4c6811d2bdb5dfdc02
dim <shintyakov@gmail.com>
2025-08-18 20:18:43 +0400

    initial commit, basic functionality works

---
+++ examples/generate_using_com.py
@@ -0,0 +1,39 @@
+# coding=utf-8
+import tempfile
+from typing import List, Callable
+from docxcomtpl import DocxComTemplate
+from docxcomtpl.com_utilities import table_inserter, heading_inserter, document_inserter
+
+def main():
+    import os
+    template_path = os.path.join(os.path.dirname(__file__), "template_com.docx")
+    template = DocxComTemplate(template_path)
+
+    data = {
+        "text": "This is a test of the docx template using COM",
+        "table": table_inserter([
+            ["Column 1", "Column 2", "Column 3"],
+            ["A", "B", "C"],
+            ["D", "E", "F"],
+            ["G", "H", "I"]
+        ]),
+        "section":[
+            {"header": heading_inserter("Section 1", 2),
+             "content": "This is the content of section 1"},
+            {"header": heading_inserter("Section 1.1", 3),
+             "content": "This is the content of section 1.1"},
+            {"header": heading_inserter("Section 2", 2),
+             "content": "This is the content of section 2"}
+        ],
+        "rtf_content": document_inserter(os.path.join(os.path.dirname(__file__), "sample.rtf")),
+    }
+    #create temporary file
+    with tempfile.NamedTemporaryFile(suffix=".docx", delete=False) as outfile:
+        pass
+
+    template.generate(data, outfile.name)
+    print("Wrote temporary file", outfile.name)
+    os.startfile(outfile.name)
+
+if __name__=="__main__":
+    main()
